5a7359e98d7a51f47511e5caab03add503f889b8
Hyebin Lee <970905hyebin@gmail.com>
2023-03-13 21:46:32 +0900

    회원가입, 로그인, 로그아웃 구현

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import auth from "@react-native-firebase/auth";
 import { NavigationContainer } from "@react-navigation/native";
