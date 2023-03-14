48a0ccb439922e4e5b591b1ecef758cd69549d9a
Hyebin Lee <970905hyebin@gmail.com>
2023-03-15 00:45:10 +0900

    로그인 회원가입 추가

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
+import auth from "@react-native-firebase/auth";
 import styled from "styled-components";
 
 const Container = styled.View``;
 const Text = styled.Text``;
+const Btn = styled.TouchableOpacity``;
+const BtnText = styled.Text``;
 
+const logOut = () => {
+  auth().signOut();
+};
 const Home = () => (
   <Container>
     <Text>HOME</Text>
+    <Btn onPress={logOut}>
+      <BtnText>Log Out</BtnText>
+    </Btn>
   </Container>
 );
 
